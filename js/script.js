ce3b139e8a9e8aa380872fd3305108732c36048a
moshiur-dev <moshiurr882@gmail.com>
2021-08-18 19:25:09 +0600

    pin match done

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function getPin() {
+    const pinDispaly = document.getElementById('pin-dispaly');
+    const pinGenerate = Math.round(Math.random() * 10000);
+    const fourDegitPin = pinGenerate + '';
+
+    if (fourDegitPin.length == 4) {
+        pinDispaly.value = pinGenerate;
+    }
+    else {
+        return getPin();
+    }
+}
+
+
+document.getElementById('pin-generate').addEventListener('click', function () {
+    getPin();
+
+});
+document.getElementById('keypad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+
+    const numberDispaly = document.getElementById('key-dispaly');
+
+    const previousNumberDisplay = numberDispaly.value;
+
+    const newNumberDispaly = previousNumberDisplay + number;
+
+    numberDispaly.value = newNumberDispaly;
+
+    if (number == 'C') {
+        numberDispaly.value = '';
+    }
+
+
+
+});
+
+document.getElementById('submit').addEventListener('click', function () {
+    const generatePinNumber = document.getElementById('pin-dispaly');
+    const generatePinNumberValue = generatePinNumber.value;
+
+    const dispalyNumber = document.getElementById('key-dispaly');
+    const dispalyNumberValue = dispalyNumber.value;
+    if (generatePinNumberValue == dispalyNumberValue) {
+        document.getElementById('show').style.display = 'block';
+        document.getElementById('hide').style.display = 'none';
+    }
+    else {
+        document.getElementById('show').style.display = 'none';
+        document.getElementById('hide').style.display = 'block';
+    }
+})
+
+
